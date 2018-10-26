4228d00cfee63838ab31d40e193b3714f6e067a5
Shivnarayan Dhaker <shivnarayan.dhaker@raweng.com>
2018-10-26 20:16:17 +0530

    minor changes

--- src/components/header.js
+++ src/components/header.js
@@ -18,9 +18,8 @@ class Header extends Component {
 											<span className="glyphicon glyphicon-remove icon-cancel" id="hide"></span> 
 									</div>
 						</div>: null}
-						{
-              //(window.location.pathname==='/')?
-              //(true)?
+						
+             
 							<header>
 								<nav className={(ribbon.ribbon_info_section.is_enable_on_only_home_page_ || ribbon.is_ribbon_disabled)&&"custom-margin"} id="navbar">
 											<div className="container">
@@ -45,34 +44,7 @@ class Header extends Component {
 													</div>
 											</div>
 									</nav>
-              </header>
-          //     :
-					// 		<header>
-					// 		<nav className="navbar secondary-nav">
-					// 				<div className="container">
-					// 						<div className="navbar-header">
-					// 								<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-					// 										<span className="sr-only">Toggle navigation</span>
-					// 										<span className="icon-bar"></span>
-					// 										<span className="icon-bar"></span>
-					// 										<span className="icon-bar"></span>
-					// 								</button>
-					// 								<a className="navbar-brand" href={header.logo_section.link}><img src={header.logo_section.logo.url} alt="" /></a>
-					// 						</div>
-					// 						<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					// 								<ul className="nav navbar-nav navbar-right">
-					// 										{
-					// 											header.nav_section.nav_menu.map((menu)=>{
-					// 												return <li key = {menu.link}><a className={(menu.title === 'Contact Us')?'btn btn-primary':''} href={menu.link}>{menu.title}</a></li>
-					// 											})
-					// 										}
-					// 								</ul>
-					// 						</div>
-					// 				</div>
-					// 		</nav>
-					// </header>
-					
-						}
+              </header>					
 				</div>
 			)
 		}
